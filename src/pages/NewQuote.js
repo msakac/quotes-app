7f24f43dd5f39cd2f5b6e399351c913868dda1c1
Martin Sakac <msakac@foi.hr>
2022-07-27 08:47:27 +0200

    Implemented rerouting on submit

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm'
 
 export default function NewQuote() {
-  
+  const history = useHistory();
+
   function addQuoteHandler(quoteData){
     console.log(quoteData);
+    history.push('/quotes');
   }
   
   return (
