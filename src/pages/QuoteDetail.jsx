940cc22221f5bb5b0d14555203e879154ead2c16
Martin Sakac <msakac@foi.hr>
2022-07-27 09:13:00 +0200

    Added button to load comments on quote with router

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, useParams } from 'react-router-dom'
+import { Link, Route, useParams } from 'react-router-dom'
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
@@ -12,13 +12,19 @@ const DUMMY_QUOTES = [
 export default function QuoteDetail() {
   const params = useParams();
   const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
-  if(!quote){
+  if (!quote) {
     return <p>No quote found</p>
   }
 
   return (
     <div>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link>
+        </div>
+      </Route>
+
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
