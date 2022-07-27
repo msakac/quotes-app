901cc7547a8efea9536e0641380aad6e28aa2936
Martin Sakac <msakac@foi.hr>
2022-07-27 10:03:47 +0200

    Adding quotes to firebase

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {useHistory} from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm'
+import {addQuote} from '../lib/api';
+import useHttp from '../hooks/use-http';
 
 export default function NewQuote() {
+  const {sendRequest, status} = useHttp(addQuote);
   const history = useHistory();
 
+  useEffect(()=>{
+    if(status === 'completed'){
+      history.push('/quotes');
+    }
+  }, [status, history])
+
   function addQuoteHandler(quoteData){
-    console.log(quoteData);
-    history.push('/quotes');
+    sendRequest(quoteData);
   }
   
   return (
-    <QuoteForm onAddQuote={addQuoteHandler}/>
+    <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler}/>
   )
 }
