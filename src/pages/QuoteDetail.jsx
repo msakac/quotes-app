a1f34a01718fd04b9f5e7597ec5e890a4d065e9c
Martin Sakac <msakac@foi.hr>
2022-07-27 08:40:48 +0200

    Added QuoteDetail page when rerouted to some quote

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { Route, useParams } from 'react-router-dom'
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Somey", text: "Learning is fun" },
+  { id: "q2", author: "Mosy", text: "Killing is fun" },
+  { id: "q3", author: "Fosy", text: "Hehoulo is fun" },
+];
+
 export default function QuoteDetail() {
-    const params = useParams();
+  const params = useParams();
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  if(!quote){
+    return <p>No quote found</p>
+  }
+
   return (
     <div>
-        <h1>Quotes</h1>
-        <h4>{params.quoteId}</h4>
-        <Route path={`/quotes/${params.quoteId}/comments`}>
-          <Comments/>
-        </Route>
+      <HighlightedQuote text={quote.text} author={quote.author}/>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </div>
   )
 }
